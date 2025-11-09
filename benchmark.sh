7fee323aed12dd40ba83b658d3ea96dd9b07f02d
gaidardzhiev <1v4n@protonmail.com>
2025-11-09 09:28:29 +0000

    fix: quote variables to prevent word splitting and globbing

--- benchmark.sh
+++ benchmark.sh
@@ -3,55 +3,55 @@
 #Copyright (C) 2025 Ivan Gaydardzhiev
 #Licensed under GPL-3.0-only
 
-base="file1.txt"
-mod="file2.txt"
-lines=512
+b="base"
+m="mod"
+l="512"
 
 fprand() {
 	tr -dc 'A-Za-z0-9' </dev/urandom | head -c 20
 }
 
-seq 1 $lines | while read n; do
-	echo "Line $n - $(fprand)"
-done > "$base"
+seq 1 "${l}" | while read n; do
+	echo "LINE "${n}" - $(fprand)"
+done > "${b}"
 
-cp "$base" "$mod"
-awk 'NR % 2 == 0 {print $0 " - MODIFIED"} NR % 2 == 1 {print $0}' "$base" > "$mod"
+cp "${b}" "${m}"
+awk 'NR % 2 == 0 {print $0 " - MODIFIED"} NR % 2 == 1 {print $0}' "${b}" > "${m}"
 
 flogos() {
-	sed -n '2s/^.\(.*\)/\1/p' "$0"
-	sed -n '3s/^.\(.*\)/\1/p' "$0"
-	sed -n '4s/^.\(.*\)/\1/p' "$0"
+	sed -n '2s/^.\(.*\)/\1/p' "${0}"
+	sed -n '3s/^.\(.*\)/\1/p' "${0}"
+	sed -n '4s/^.\(.*\)/\1/p' "${0}"
 	printf "\n"
 }
 
 ftime() {
-	c=$1
-	f1=$2
-	f2=$3
+	c="${1}"
+	f1="${2}"
+	f2="${3}"
 	s=$(date +%s%N)
 	eval "$c '$f1' '$f2'" >/dev/null 2>&1
 	d=$(date +%s%N)
 	e=$((d - s))
-	printf "%s execution time: %d nanoseconds\n" "$c" "$e" >&2
-	echo "$e"
+	printf "%s execution time: %d nanoseconds\n" "${c}" "${e}" >&2
+	echo "${e}"
 }
 
-[ "$#" -ne 0 ] && {
-	printf "usage: $0\n"
+[ "${#}" -ne 0 ] && {
+	printf "usage: %s\n" "${0}"
 	exit 1
 }
 
 flogos
 
-file1=$base
-file2=$mod
+z="${b}"
+x="${m}"
 
-p=$(ftime ./diff "$file1" "$file2")
-q=$(ftime "diff -u" "$file1" "$file2")
+p=$(ftime ./diff "${z}" "${x}")
+q=$(ftime "diff -u" "${z}" "${x}")
 
-[ "$p" -lt "$q" ] && f="custom diff is faster by $((q - p)) nanoseconds" || f="systems diff is faster by $((p - q)) nanoseconds"
+[ "${p}" -lt "${q}" ] && f="custom diff is faster by $((q - p)) nanoseconds" || f="systems diff is faster by $((p - q)) nanoseconds"
 
-printf "\nin this test %s\n" "$f"
+printf "\nin this test %s\n" "${f}"
 
-rm -f $base $mod
+rm -f "${base}" "${mod}"
